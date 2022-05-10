6dfb2f91f4517788a1192b5d696294a3f45ce36e
unknown <melih.gulec14@gmail.com>
2022-05-10 17:19:17 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/Rouvie/event_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.Rouvie.event_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MainScreen());
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
